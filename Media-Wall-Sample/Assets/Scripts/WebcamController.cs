7e8fa5621730694091e64a1ad9b5c990f72ce904
AJ <a.j.woodward53@gmail.com>
2023-08-23 16:50:52 +0100

    updated webcam controller

--- Media-Wall-Sample/Assets/Scripts/WebcamController.cs
+++ Media-Wall-Sample/Assets/Scripts/WebcamController.cs
@@ -4,35 +4,42 @@ using UnityEngine.UI;
 
 public class WebcamController : MonoBehaviour
 {
+    public int deviceIndex = 0;
+    private bool hasUserAuthorization = false;
+    
     private WebCamTexture webcamTexture;
     private Renderer rendererComponent;
     private RawImage rawImageComponent;
 
     IEnumerator Start()
     {
+        //Loop Authorization Request until Authorized
         yield return Application.RequestUserAuthorization(UserAuthorization.WebCam);
-        while (Application.HasUserAuthorization(UserAuthorization.WebCam) == false)
-        {
-            yield return new WaitForSeconds(0.25f);
-            yield return Application.RequestUserAuthorization(UserAuthorization.WebCam);
-            Debug.Log("Authorisation Requested");
-        }
+        //while (Application.HasUserAuthorization(UserAuthorization.WebCam) == false)
+        //{
+        //    yield return new WaitForSeconds(0.25f);
+        //    yield return Application.RequestUserAuthorization(UserAuthorization.WebCam);
+        //    Debug.Log("Authorisation Requested");
+        //}
+        hasUserAuthorization = Application.HasUserAuthorization(UserAuthorization.WebCam);
 
-        Debug.Log(Application.HasUserAuthorization(UserAuthorization.WebCam));
-        if (Application.HasUserAuthorization(UserAuthorization.WebCam))
+        hasUserAuthorization = true;
+
+        Debug.Log(hasUserAuthorization);
+        if (hasUserAuthorization)
         {
             // Check for Renderer
             rendererComponent = GetComponent<Renderer>();
             // Check for CanvasRenderer
             rawImageComponent = GetComponent<RawImage>();
 
-            webcamTexture = new WebCamTexture();
-
             WebCamDevice[] devices = WebCamTexture.devices;
             for (int i = 0; i < devices.Length; i++)
             {
                 Debug.Log(devices[i].name);
             }
+
+            webcamTexture = new WebCamTexture(devices[2].name);
 
             // Perform swapping based on the availability of components
             if (rendererComponent != null)
